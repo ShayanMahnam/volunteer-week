5ac19ec324c5caa81d6c9dbd74c4b48f87da357f
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-05-23 20:39:28 +0100

    Adding picture and photos, updating Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,165 +1,153 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Marquee from "react-fast-marquee";
+import Image from "next/image";
 
 const Carousel: React.FC = () => {
+  //creating array for photos iterating
+  const photoNumber = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+  ];
+
+  //another rotate 7.25 deg
   return (
-    <div className="relative py-12 flex items-center overflow-x-hidden">
-      <Marquee>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/1.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/2.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/3.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/4.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/5.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/6.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/7.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/8.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/9.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/10.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/11.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/12.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/13.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/14.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/15.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/16.webp")',
-            }}
-          ></div>
-        </div>
-        <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage:
-                'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684858361/volunteers/17.webp")',
-            }}
-          ></div>
-        </div>
-      </Marquee>
+    <div className="relative py-12">
+      <div className="absolute rope top-28"></div>
+      <div className="relative flex items-center overflow-x-hidden">
+        <Marquee>
+          {photoNumber.map((num) => (
+            <div key={num} className="relative w-60 h-96 m-6">
+              <Image
+                className={`absolute top-0 left-1/2 transform -translate-x-1/2 ${
+                  num % 2 === 0 ? "rotate-[-5.67deg]" : "rotate-[7.25deg]"
+                } z-10 w-[100px] h-auto`}
+                src="/media/pictures/Pin.webp"
+                alt="pin"
+                width={100}
+                height={100}
+              />
+              <div
+                className={`absolute z-0 top-[4.5rem] px-3 pt-3 pb-10 w-60 h-72 border rounded bg-white shadow-xl ${
+                  num % 2 === 0 ? "rotate-1" : "-rotate-1"
+                }`}
+              >
+                <div
+                  className="w-full h-full bg-cover bg-center"
+                  style={{
+                    backgroundImage: `url(/media/photos/Photo_${num}.webp)`,
+                  }}
+                ></div>
+              </div>
+            </div>
+          ))}
+          {/* <div className="relative w-60 h-96">
+            <img
+              className="absolute top-0 left-1/2 transform -translate-x-1/2 rotate-[-5.67deg] z-10 w-[100px] h-auto"
+              src="/media/img/Photo_pin.webp"
+              alt="pin"
+            />
+            <div className="absolute z-0 top-12 pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
+              <div
+                className="w-full h-full bg-cover bg-center"
+                style={{
+                  backgroundImage:
+                    'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/2_cl8bxd.webp")',
+                }}
+              ></div>
+            </div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436017/volunteers/10_ppg9rw.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/9_fhlnns.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/1_tct4vb.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/4_fgkfo9.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/3_l1rc0l.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/5_frpq5o.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/11_djbrih.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/7_ngtfe9.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl -rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/6_ce55hw.webp")',
+              }}
+            ></div>
+          </div>
+          <div className="pb-10 px-3 pt-3 m-6 w-60 h-72 border rounded bg-white shadow-xl rotate-1">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/dhfp1rz0p/image/upload/f_auto,q_auto/v1684436018/volunteers/8_b5hlzt.webp")',
+              }}
+            ></div>
+          </div> */}
+        </Marquee>
+      </div>
     </div>
   );
 };
