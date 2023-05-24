2b1cb17c39a0e2a1b830489e9e0b7d5ceeaaaae8
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-05-24 17:49:15 +0100

    Updating pictures, styling GreenBoard

--- components/GreenBoard.tsx
+++ components/GreenBoard.tsx
@@ -1,4 +1,5 @@
 import DragAndDropArea from "./DragAndDropArea";
+import Image from "next/image";
 
 const GreenBoard = () => {
   return (
@@ -7,6 +8,13 @@ const GreenBoard = () => {
       <div className="absolute top-0 z-0 w-full h-full">
         <DragAndDropArea />
       </div>
+      <Image
+        className="absolute bottom-[14px] left-1/2 transform -translate-x-1/2  z-20 w-2/3 h-auto md:w-1/3"
+        src="/media/pictures/green_board_part.webp"
+        alt="board part"
+        width={1000}
+        height={1000}
+      />
     </div>
   );
 };
