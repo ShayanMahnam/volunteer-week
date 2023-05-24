2bf27f93c1698c258eed1f6bb08db4a7224a7dee
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-05-24 16:18:39 +0100

    add billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import DragAndDropArea from "@/components/DragAndDropArea";
 import Carousel from "@/components/Carousel";
 import Drawer from "@/components/Drawer";
 import Board from "@/components/Board";
+import Billboard from "@/components/Billboard";
 
 export default function Home() {
   return (
     <>
+      <Billboard />
       <Board />
       <Carousel />
       <h1 className="text-center">Trainees Messages</h1>
