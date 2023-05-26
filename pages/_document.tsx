83e1c3308979c511cb9e0c59fa6bf7df180127d5
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-05-26 12:37:59 +0100

    add title and icon and meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* eslint-disable @next/next/no-title-in-document-head */
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Volunteer Week</title>
+          <meta name="description" content="Thank you amazing cyf volunteers" />
+          <link rel="icon" href="/thanks.svg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
