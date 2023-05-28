1eed2e1910225889f65b747a0df061bd9b1f846c
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-05-28 15:48:53 +0100

    Adding ScrollToTopButton

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Carousel from "@/components/Carousel";
 import Drawer from "@/components/Drawer";
 import Board from "@/components/Board";
 import Billboard from "@/components/Billboard";
+import ScrollToTopButton from "@/components/ScrollToTopButton";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Carousel />
       <h1 className="text-center text-black">Trainees Messages</h1>
       <Drawer />
+      <ScrollToTopButton />
     </>
   );
 }
