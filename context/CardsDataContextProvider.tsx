dc57bf7bc6476205f1a960353083732884837856
Baz Murphy <bazmurphy@gmail.com>
2023-05-31 15:25:32 +0100

    injection of static card colours separate from json

--- context/CardsDataContextProvider.tsx
+++ context/CardsDataContextProvider.tsx
@@ -3,7 +3,9 @@ import CardsDataContext from "./CardsDataContext";
 import cardsDataJson from "../cards.json";
 
 const CardsDataContextProvider = ({children}) => {
-  const cardsData = cardsDataJson.sort(((a, b) => (a.subject.localeCompare(b.subject))));
+  const cardColours = ["white", "green", "pink", "yellow", "blue", "purple"];
+  // sort by subject and then inject a color key/value that remains static
+  const cardsData = cardsDataJson.sort(((a, b) => (a.subject.localeCompare(b.subject)))).map((card, index) => ({...card, color: `card-bg-${cardColours[index % cardColours.length]}`}))
 
   const [subject, setSubject] = useState(null);
 
