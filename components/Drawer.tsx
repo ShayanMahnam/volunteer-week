14f0e76a958e55a25ef1edcd45117fd197ed1d35
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-05-31 20:03:57 +0100

    add active class to drawer list

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-// import useContext and the CardsDataContext
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CardsDataContext } from "../context/CardsDataContext";
-// (!) this component is no longer neccessary:
-// import DrawerModalMessage from "./DrawerModalMessage";
 import GreenBoard from "./GreenBoard";
 
 const Drawer: React.FC = () => {
+  const { cardsData, setSubject } = useContext(CardsDataContext);
+  const [activeSubject, setActiveSubject] = useState("All Messages");
 
-  // bring in cardsData and setSubject method from CardsDataContext
-  const { cardsData, setSubject} = useContext(CardsDataContext);
-
-  // this extracts all the unique subjects from the cardsData, and arranges them with a Reset option (1st), All The Volunteers (2nd), followed by an Alphabetical List of Volunteers
   const uniqueSubjects = [
-        "Reset",
-        "All The Volunteers",
-        ...Array.from(
-          new Set(
-            cardsData
-              .filter((obj) => obj.subject !== "All The Volunteers")
-              .map((obj) => obj.subject)
-              .sort((a, b) => a.localeCompare(b))
-          )
-        ),
-      ];
+    "All Messages",
+    "All The Volunteers",
+    ...Array.from(
+      new Set(
+        cardsData
+          .filter((obj) => obj.subject !== "All The Volunteers")
+          .map((obj) => obj.subject)
+          .sort((a, b) => a.localeCompare(b))
+      )
+    ),
+  ];
 
-  // if "Reset" is clicked, make the subject "", otherwise set the Subject to the clicked subject (Volunteer Name)
-  const handleListItemClick = (subject: string) => subject === "Reset" ? setSubject("") : setSubject(subject);
+  const handleListItemClick = (subject: string) => {
+    if (subject === "All Messages") {
+      setSubject("");
+    } else {
+      setSubject(subject);
+    }
+    setActiveSubject(subject); // Update the activeSubject state
+  };
 
   return (
     <div className="drawer">
@@ -44,10 +44,12 @@ const Drawer: React.FC = () => {
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer" className="drawer-overlay"></label>
-        <ul className="custom-sidebar menu p-4 w-80 bg-base-100 text-base-content ">
+        <ul className="custom-sidebar menu p-4 w-80 bg-base-100 text-base-content">
           {uniqueSubjects.map((subject, index) => (
             <li
-              className="hover:text-black"
+              className={`hover:text-black ${
+                activeSubject === subject ? "text-black" : ""
+              }`}
               key={index}
               onClick={() => handleListItemClick(subject)}
             >
